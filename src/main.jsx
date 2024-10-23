a65383da924b90e526c633c646e98dae0432dd07
anacrsoares <anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.com>
2024-10-23 16:54:47 -0300

    adicionando o basename no BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import MovieDetails from "./pages/MovieDetails";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/api-frontend-tmdb">
     <Routes>
       <Route path="/api-frontend-tmdb/" element={<Home />} />
       <Route path="/movie/:id" element={<MovieDetails />} />
