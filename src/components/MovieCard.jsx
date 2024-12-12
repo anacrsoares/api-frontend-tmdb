474e43a2310e141edfa6ca572ab462c543a2e978
anacrsoares <anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.com>
2024-12-12 09:14:01 -0300

    wip: tp4 - criando o botão de favoritos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import "./movieCard.css";
 import StarRating from "./StarRating";
 import { Link } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { useState } from "react";
+import { pink, red } from "@mui/material/colors";
 
 const posterImage = import.meta.env.VITE_IMG;
 
 export default function MovieCard({ index, movie }) {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleFavoriteClick = () => {
+    setIsFavorited(!isFavorited);
+  };
+
   return (
     <>
       <li key={index}>
@@ -24,6 +35,17 @@ export default function MovieCard({ index, movie }) {
                 rating={movie.vote_average}
               />
             </h2>
+
+            {/* Botão de Favorito */}
+            <IconButton
+              onClick={handleFavoriteClick}
+              sx={{ justifyContent: "flex-end" }}
+            >
+              {isFavorited ? (
+                <FavoriteIcon sx={{ fontSize: 40, color: red[900] }} />
+              ) : null}
+            </IconButton>
+
             <div className="hidden-content">
               {movie.overview && (
                 <p className="description-overview">
@@ -38,6 +60,13 @@ export default function MovieCard({ index, movie }) {
                   Ver mais
                 </Link>
               </button>
+
+              {/* Botão de Favorito */}
+              <IconButton onClick={handleFavoriteClick}>
+                {isFavorited ? null : (
+                  <FavoriteBorderIcon sx={{ fontSize: 40, color: red[900] }} />
+                )}
+              </IconButton>
             </div>
           </div>
         </div>
